01d8149e2288fbc35510af71aaad80eac4862668
Yuhao Wang <yuhaow97@gmail.com>
2021-04-15 19:39:14 +0800

    refactor: mv cores to core

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 from .config import import_config
-from .runner import Runner
-from .launcher import launch_training, launch_inference_runner
-from .meter_pool import MeterPool, AvgMeter
+from .core.runner import Runner
+from .core.launcher import launch_training, launch_inference_runner
+from .core.meter_pool import MeterPool, AvgMeter
+from .core.dist import *
 from .utils import *
