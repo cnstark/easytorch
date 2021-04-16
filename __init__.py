ce0785fb1aa233e989adcb9eb0259b79abc38449
Yuhao Wang <yuhaow97@gmail.com>
2021-04-16 11:15:47 +0800

    modify __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,3 @@
 from .config import import_config
-from .core.runner import Runner
-from .core.launcher import launch_training, launch_inference_runner
-from .core.meter_pool import MeterPool, AvgMeter
-from .core.dist import *
+from .core import *
 from .utils import *
