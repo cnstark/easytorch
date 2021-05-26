166c16fed2cdee131fa7c833d57b997b433a87e2
Yuhao Wang <yuhaow97@gmail.com>
2021-05-26 21:30:44 +0800

    feature: doc for config.py (unfinished)

--- config.py
+++ config.py
@@ -1,9 +1,67 @@
+"""Everything is based on config.
+
+`Config` is the set of all configurations. `Config` is is implemented by `dict`, We recommend using `EasyDict`.
+
+Look at the following example:
+
+cfg.py
+
+```python
+import os
+from easydict import EasyDict
+
+from my_runner import MyRunner
+
+CFG = EasyDict()
+
+CFG.DESC = 'my net'  # customized description
+CFG.RUNNER = MyRunner
+CFG.GPU_NUM = 1
+
+CFG.MODEL = EasyDict()
+CFG.MODEL.NAME = 'my_net'
+
+CFG.TRAIN = EasyDict()
+
+CFG.TRAIN.NUM_EPOCHS = 100
+CFG.TRAIN.CKPT_SAVE_DIR = os.path.join(
+    'checkpoints',
+    '_'.join([CFG.MODEL.NAME, str(CFG.TRAIN.NUM_EPOCHS)])
+)
+CFG.TRAIN.CKPT_SAVE_STRATEGY = None
+
+CFG.TRAIN.OPTIM = EasyDict()
+CFG.TRAIN.OPTIM.TYPE = 'SGD'
+CFG.TRAIN.OPTIM.PARAM = {
+    'lr': 0.002,
+    'momentum': 0.1,
+}
+
+CFG.TRAIN.DATA = EasyDict()
+CFG.TRAIN.DATA.BATCH_SIZE = 4
+CFG.TRAIN.DATA.DIR = './my_data'
+CFG.TRAIN.DATA.SHUFFLE = True
+CFG.TRAIN.DATA.PIN_MEMORY = True
+CFG.TRAIN.DATA.PREFETCH = True
+
+CFG.VAL = EasyDict()
+
+CFG.VAL.INTERVAL = 1
+
+CFG.VAL.DATA = EasyDict()
+CFG.VAL.DATA.DIR = 'mnist_data'
+
+```
+
+All configurations consists of two parts:
+the configurations that affects the training results and the configurations that does not affect the training results
+
+"""
+
 import types
 import hashlib
 
-
 MD5_EXCEPT_FLAG = '_md5_except'
-
 
 DEFAULT_MD5_EXCEPT_KEYS = [
     'TRAIN.CKPT_SAVE_STRATEGY',
@@ -15,6 +73,15 @@ DEFAULT_MD5_EXCEPT_KEYS = [
 
 
 def except_dict_keys(cfg: dict, except_keys: set):
+    """
+
+    Args:
+        cfg:
+        except_keys:
+
+    Returns:
+
+    """
     cfg_copy = cfg.copy()
 
     if cfg_copy.get(MD5_EXCEPT_FLAG) is not None:
@@ -24,12 +91,12 @@ def except_dict_keys(cfg: dict, except_keys: set):
     dict_list = []
     for k, v in cfg.items():
         if isinstance(v, dict):
-            sub_except_keys = []
+            sub_except_keys = set([])
             for except_key in except_keys:
                 if k == except_key:
                     pop_list.append(k)
                 elif except_key.find(k) == 0 and except_key[len(k)] == '.':
-                    sub_except_keys.append(except_key[len(k) + 1:])
+                    sub_except_keys.add(except_key[len(k) + 1:])
             if len(sub_except_keys) != 0:
                 new_v = except_dict_keys(v, sub_except_keys)
                 dict_list.append((k, new_v))
@@ -47,7 +114,7 @@ def except_dict_keys(cfg: dict, except_keys: set):
     return cfg_copy
 
 
-def config_str(cfg: dict, indent: str=''):
+def config_str(cfg: dict, indent: str = ''):
     s = ''
     for k, v in cfg.items():
         if isinstance(v, dict):
@@ -81,7 +148,7 @@ def save_config(cfg: dict, file_path: str):
         f.write(content)
 
 
-def import_config(path: str, verbose: bool=True):
+def import_config(path: str, verbose: bool = True):
     if path.find('.py') != -1:
         path = path[:path.find('.py')].replace('/', '.')
     cfg_name = path.split('.')[-1]
