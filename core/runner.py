ee6825aca1c2532deda90ed125d1ad31d5c67ae1
Tony Stark <45590791+cnstark@users.noreply.github.com>
2021-05-26 16:25:22 +0800

    feature: split common functions (#14)
    * feature: add checkpoint funcs
    
    * fix: fix param bug
    
    * feature: build dataloader
    
    * feature: change name
    
    * feature: split optim
    
    * fix: change names
    
    * feature: add note
    
    * feature: add note
    
    * feature: add note
    
    * fix: fix note
    
    * fix: change return type to dict
    
    * fix: change _create_model to build_model
    
    * feature: change DEFAULT_MD5_EXCEPT_KEYS
    
    * feature: change some methods name

--- core/runner.py
+++ core/runner.py
@@ -1,18 +1,17 @@
 import os
 import time
-import glob
-from abc import ABCMeta, abstractmethod, abstractstaticmethod
+from abc import ABCMeta, abstractmethod
 
 import torch
 from torch.nn.parallel import DistributedDataParallel as DDP
-from torch import optim
-from torch.optim import lr_scheduler
 from torch.utils.tensorboard import SummaryWriter
 
 from .meter_pool import MeterPool
+from .checkpoint import get_ckpt_dict, load_ckpt, save_ckpt, backup_last_ckpt, clear_ckpt
+from .data_loader import build_data_loader, build_data_loader_ddp
+from .optimizer_builder import build_optim, build_lr_scheduler
 from ..config import config_md5, save_config
 from ..utils import get_logger, get_rank, is_master, master_only
-from ..easyoptim import easy_lr_scheduler
 
 
 class Runner(metaclass=ABCMeta):
@@ -30,7 +29,7 @@ class Runner(metaclass=ABCMeta):
         self.logger = get_logger('easytorch')
 
         # create model
-        self.model = self._create_model(cfg)
+        self.model = self.build_model(cfg)
 
         # declare optimizer and lr_scheduler
         self.optim = None
@@ -41,68 +40,67 @@ class Runner(metaclass=ABCMeta):
         self.val_data_loader = None
 
         # declare meter pool
-        self._meter_pool = None
+        self.meter_pool = None
 
         # declare tensorboard_writer
         self.tensorboard_writer = None
 
-    @abstractstaticmethod
+    @staticmethod
+    @abstractmethod
     def define_model(cfg):
         pass
 
-    @abstractstaticmethod
-    def define_train_data_loader(cfg):
+    @staticmethod
+    @abstractmethod
+    def build_train_dataset(cfg: dict):
         pass
 
-    @abstractstaticmethod
-    def define_train_data_loader_ddp(cfg):
+    @staticmethod
+    @abstractmethod
+    def build_val_dataset(cfg: dict):
         pass
 
-    @abstractstaticmethod
-    def define_val_data_loader(cfg):
-        pass
+    def build_train_data_loader(self, cfg: dict):
+        dataset = self.build_train_dataset(cfg)
+        if torch.distributed.is_initialized():
+            return build_data_loader_ddp(dataset, cfg.TRAIN.DATA)
+        else:
+            return build_data_loader(dataset, cfg.TRAIN.DATA)
 
-    def _create_model(self, cfg):
+    def build_val_data_loader(self, cfg: dict):
+        dataset = self.build_val_dataset(cfg)
+        return build_data_loader(dataset, cfg.VAL.DATA)
+
+    def build_model(self, cfg):
         model = self.define_model(cfg)
         model = model.cuda()
         if torch.distributed.is_initialized():
             model = DDP(model, device_ids=[get_rank()])
         return model
 
-    @staticmethod
-    def _create_optim(optim_cfg, model):
-        Optim = getattr(optim, optim_cfg.TYPE)
-        optim_param = optim_cfg.PARAM.copy()
-        optimizer = Optim(model.parameters(), **optim_param)
-        return optimizer
+    def get_ckpt_path(self, epoch: int):
+        epoch_str = str(epoch).zfill(len(str(self.num_epochs)))
+        ckpt_name = '{}_{}.pt'.format(self.model_name, epoch_str)
+        return os.path.join(self.ckpt_save_dir, ckpt_name)
 
-    @staticmethod
-    def _create_lr_scheduler(lr_scheduler_cfg, optim):
-        if hasattr(lr_scheduler, lr_scheduler_cfg.TYPE):
-            Scheduler = getattr(lr_scheduler, lr_scheduler_cfg.TYPE)
-        else:
-            Scheduler = getattr(easy_lr_scheduler, lr_scheduler_cfg.TYPE)
-        scheduler_param = lr_scheduler_cfg.PARAM.copy()
-        scheduler_param['optimizer'] = optim
-        scheduler = Scheduler(**scheduler_param)
-        return scheduler
+    def save_model(self, epoch):
+        ckpt_dict = get_ckpt_dict(self.model, self.optim, epoch)
 
-    def _save_model(self, epoch):
-        if isinstance(self.model, DDP):
-            _model = self.model.module
-        else:
-            _model = self.model
+        # backup last epoch
+        last_ckpt_path = self.get_ckpt_path(epoch - 1)
+        backup_last_ckpt(last_ckpt_path, epoch, self.ckpt_save_strategy)
 
-        checkpoint_dict = {
-            'epoch': epoch,
-            'model_state_dict': _model.state_dict(),
-            'optim_state_dict': self.optim.state_dict()
-        }
-        self._save_checkpoint(epoch, checkpoint_dict)
+        # save ckpt
+        ckpt_path = self.get_ckpt_path(epoch)
+        save_ckpt(ckpt_dict, ckpt_path, self.logger)
 
-    def _load_model_resume(self, strict=True):
+        # clear ckpt every 10 epoch or in the end
+        if epoch % 10 == 0 or epoch == self.num_epochs:
+            clear_ckpt(self.ckpt_save_dir)
+
+    def load_model_resume(self, strict=True):
         try:
-            checkpoint_dict = self._load_checkpoint()
+            checkpoint_dict = load_ckpt(self.ckpt_save_dir, logger=self.logger)
             if isinstance(self.model, DDP):
                 self.model.module.load_state_dict(checkpoint_dict['model_state_dict'], strict=strict)
             else:
@@ -115,64 +113,15 @@ class Runner(metaclass=ABCMeta):
         except (IndexError, OSError, KeyError):
             pass
 
-    def _load_model_finetune(self, ckpt_path, strict=True):
-        checkpoint_dict = self._load_checkpoint(ckpt_path)
-        if isinstance(self.model, DDP):
-            self.model.module.load_state_dict(checkpoint_dict['model_state_dict'], strict=strict)
-        else:
-            self.model.load_state_dict(checkpoint_dict['model_state_dict'], strict=strict)
-        self.logger.info('start finetuning')
-
-    def load_model_inference(self, ckpt_path=None, strict=True):
+    def load_model(self, ckpt_path=None, strict=True):
         try:
-            checkpoint_dict = self._load_checkpoint(ckpt_path)
-            self.model.load_state_dict(checkpoint_dict['model_state_dict'], strict=strict)
-        except (IndexError, OSError, KeyError):
-            raise OSError('ckpt file does not exist')
-
-    def _load_checkpoint(self, ckpt_path=None):
-        if ckpt_path is None:
-            ckpt_path = self._get_ckpt_path()
-        self.logger.info('load ckpt from \'{}\''.format(ckpt_path))
-        return torch.load(ckpt_path, map_location='cuda:{}'.format(get_rank()))
-
-    def _save_checkpoint(self, epoch, checkpoint_dict):
-        last_epoch = epoch - 1
-
-        # ckpt save strategy
-        if self.ckpt_save_strategy is None:
-            remove_last_epoch = True
-        elif isinstance(self.ckpt_save_strategy, int) and last_epoch % self.ckpt_save_strategy != 0:
-            remove_last_epoch = True
-        elif isinstance(self.ckpt_save_strategy, list) and not last_epoch in self.ckpt_save_strategy:
-            remove_last_epoch = True
-        else:
-            remove_last_epoch = False
-
-        # rename last ckpt to .bak
-        if remove_last_epoch and last_epoch != 0:
-            last_epoch_str = str(last_epoch).zfill(len(str(self.num_epochs)))
-            last_ckpt_name = '{}_{}.pt'.format(self.model_name, last_epoch_str)
-            last_ckpt_path = os.path.join(self.ckpt_save_dir, last_ckpt_name)
-            os.rename(last_ckpt_path, last_ckpt_path + '.bak')
-
-        # save ckpt
-        epoch_str = str(epoch).zfill(len(str(self.num_epochs)))
-        checkpoint_name = '{}_{}.pt'.format(self.model_name, epoch_str)
-        checkpoint_path = os.path.join(self.ckpt_save_dir, checkpoint_name)
-        torch.save(checkpoint_dict, checkpoint_path)
-        self.logger.info('ckpt {} saved'.format(checkpoint_path))
-
-        # clear ckpt every 10 epoch or in the end
-        if epoch % 10 == 0 or epoch == self.num_epochs:
-            ckpt_list = glob.glob(os.path.join(self.ckpt_save_dir, '*.pt.bak'))
-            for ckpt in ckpt_list:
-                os.remove(ckpt)
-
-    def _get_ckpt_path(self):
-        ckpt_list = glob.glob(os.path.join(self.ckpt_save_dir, '*.pt'))
-        ckpt_list.sort()
-        return ckpt_list[-1]
+            checkpoint_dict = load_ckpt(self.ckpt_save_dir, ckpt_path=ckpt_path, logger=self.logger)
+            if isinstance(self.model, DDP):
+                self.model.module.load_state_dict(checkpoint_dict['model_state_dict'], strict=strict)
+            else:
+                self.model.load_state_dict(checkpoint_dict['model_state_dict'], strict=strict)
+        except (IndexError, OSError):
+            raise OSError('Ckpt file does not exist')
 
     def train(self, cfg):
         self.init_training(cfg)
@@ -180,9 +129,9 @@ class Runner(metaclass=ABCMeta):
         # train loop
         for epoch_index in range(self.start_epoch, self.num_epochs):
             epoch = epoch_index + 1
-            self._on_epoch_start(epoch)
+            self.on_epoch_start(epoch)
             epoch_start_time = time.time()
-            # start traning
+            # start training
             self.model.train()
             for iter_index, data in enumerate(self.train_data_loader):
                 loss = self.train_iters(epoch, iter_index, data)
@@ -193,7 +142,7 @@ class Runner(metaclass=ABCMeta):
                 self.scheduler.step()
 
             epoch_end_time = time.time()
-            self._on_epoch_end(epoch, epoch_end_time - epoch_start_time)
+            self.on_epoch_end(epoch, epoch_end_time - epoch_start_time)
         self.on_training_end()
 
     @abstractmethod
@@ -236,38 +185,36 @@ class Runner(metaclass=ABCMeta):
 
         # init meter_pool
         if is_master():
-            self._meter_pool = MeterPool()
+            self.meter_pool = MeterPool()
 
         # train data loader
-        if torch.distributed.is_initialized():
-            self.train_data_loader = self.define_train_data_loader_ddp(cfg)
-        else:
-            self.train_data_loader = self.define_train_data_loader(cfg)
+        self.train_data_loader = self.build_train_data_loader(cfg)
         self.register_epoch_meter('train_time', 'train', '{:.2f} (s)', plt=False)
 
         # val config and val data loader
         if hasattr(cfg, 'VAL'):
             if hasattr(cfg.VAL, 'INTERVAL'):
                 self.val_interval = cfg.VAL.INTERVAL
-            self.val_data_loader = self.define_val_data_loader(cfg)
+            self.val_data_loader = self.build_val_data_loader(cfg)
             self.register_epoch_meter('val_time', 'val', '{:.2f} (s)', plt=False)
 
         # create optim
-        self.optim = self._create_optim(cfg.TRAIN.OPTIM, self.model)
+        self.optim = build_optim(cfg.TRAIN.OPTIM, self.model)
         self.logger.info('set optim: ' + str(self.optim))
 
         # create lr_scheduler
         if hasattr(cfg.TRAIN, 'LR_SCHEDULER'):
-            self.scheduler = self._create_lr_scheduler(cfg.TRAIN.LR_SCHEDULER, self.optim)
+            self.scheduler = build_lr_scheduler(cfg.TRAIN.LR_SCHEDULER, self.optim)
             self.logger.info('set lr_scheduler: ' + str(self.scheduler))
             self.register_epoch_meter('lr', 'train', '{:.2e}')
 
-        # finetune
+        # fine tune
         if hasattr(cfg.TRAIN, 'FINETUNE_FROM'):
-            self._load_model_finetune(cfg.TRAIN.FINETUNE_FROM)
+            self.load_model(cfg.TRAIN.FINETUNE_FROM)
+            self.logger.info('start fine tuning')
 
         # resume
-        self._load_model_resume()
+        self.load_model_resume()
 
         # init tensorboard(after resume)
         if is_master():
@@ -277,7 +224,7 @@ class Runner(metaclass=ABCMeta):
             )
 
     @master_only
-    def _on_epoch_start(self, epoch):
+    def on_epoch_start(self, epoch):
         # print epoch num
         self.logger.info('epoch {:d} / {:d}'.format(epoch, self.num_epochs))
         # update lr meter
@@ -285,7 +232,7 @@ class Runner(metaclass=ABCMeta):
             self.update_epoch_meter('lr', self.scheduler.get_lr()[0])
 
     @master_only
-    def _on_epoch_end(self, epoch, epoch_time):
+    def on_epoch_end(self, epoch, epoch_time):
         # epoch time
         self.update_epoch_meter('train_time', epoch_time)
         # print train meters
@@ -301,7 +248,7 @@ class Runner(metaclass=ABCMeta):
         # tensorboard plt meters
         self.plt_epoch_meters(epoch)
         # save model
-        self._save_model(epoch)
+        self.save_model(epoch)
         # reset meters
         self.reset_epoch_meters()
 
@@ -312,20 +259,20 @@ class Runner(metaclass=ABCMeta):
 
     @master_only
     def register_epoch_meter(self, name, meter_type, fmt='{:f}', plt=True):
-        self._meter_pool.register(name, meter_type, fmt, plt)
+        self.meter_pool.register(name, meter_type, fmt, plt)
 
     @master_only
     def update_epoch_meter(self, name, value):
-        self._meter_pool.update(name, value)
+        self.meter_pool.update(name, value)
 
     @master_only
     def print_epoch_meters(self, meter_type):
-        self._meter_pool.print_meters(meter_type, self.logger)
+        self.meter_pool.print_meters(meter_type, self.logger)
 
     @master_only
     def plt_epoch_meters(self, epoch):
-        self._meter_pool.plt_meters(epoch, self.tensorboard_writer)
+        self.meter_pool.plt_meters(epoch, self.tensorboard_writer)
 
     @master_only
     def reset_epoch_meters(self):
-        self._meter_pool.reset()
+        self.meter_pool.reset()
