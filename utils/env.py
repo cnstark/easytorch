0a66124b744d728d5526f3bbae0861ebcbe88186
Yuhao Wang <yuhaow97@gmail.com>
2021-06-14 17:48:12 +0800

    env notes

--- utils/env.py
+++ utils/env.py
@@ -5,18 +5,34 @@ import torch
 from .logging import get_logger
 
 
-def set_gpus(gpus):
+def set_gpus(gpus: str):
+    """Set environment variable `CUDA_VISIBLE_DEVICES` to select GPU devices.
+
+    Examples:
+        set_gpus('0,1,2,3')
+
+    Args:
+        gpus (str): environment variable `CUDA_VISIBLE_DEVICES` value
+    """
+
     if gpus is not None:
         os.environ["CUDA_VISIBLE_DEVICES"] = gpus
 
 
-def set_tf32_mode(tf32_mode):
+def set_tf32_mode(tf32_mode: bool):
+    """Set tf32 mode on Ampere gpu when torch version >= 1.7.0 and cuda version >= 11.0.
+    See https://pytorch.org/docs/stable/notes/cuda.html#tf32-on-ampere
+
+    Args:
+        tf32_mode (bool): set to ``True`` to enable tf32 mode.
+    """
+
     logger = get_logger('easytorch-env')
     if torch.__version__ >= '1.7.0':
         if tf32_mode:
             logger.info('Enable TF32 mode')
         else:
-            # disable tf32 mode on A100
+            # disable tf32 mode on Ampere gpu
             torch.backends.cuda.matmul.allow_tf32 = False
             torch.backends.cudnn.allow_tf32 = False
             logger.info('Disable TF32 mode')
