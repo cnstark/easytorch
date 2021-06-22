47114d59ab570ab037fac820abd54ea0304c093a
Yuhao Wang <yuhaow97@gmail.com>
2021-06-22 13:58:25 +0800

    runner notes

--- core/runner.py
+++ core/runner.py
@@ -4,6 +4,8 @@ import logging
 from abc import ABCMeta, abstractmethod
 
 import torch
+from torch import nn
+from torch.utils.data import Dataset, DataLoader
 from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.utils.tensorboard import SummaryWriter
 
@@ -49,6 +51,15 @@ class Runner(metaclass=ABCMeta):
 
     def init_logger(self, logger: logging.Logger = None, logger_name: str = None,
                     log_file_name: str = None, log_level: int = logging.INFO):
+        """Initialize logger.
+
+        Args:
+            logger (logging.Logger, optional): specified logger.
+            logger_name (str, optional): specified name of logger.
+            log_file_name (str, optional): logger file name.
+            log_level (int, optional): log level, default is INFO.
+        """
+
         if logger is not None:
             self.logger = logger
         elif logger_name is not None:
@@ -58,46 +69,139 @@ class Runner(metaclass=ABCMeta):
             else:
                 log_file_path = None
             self.logger = get_logger(logger_name, log_file_path, log_level)
+        else:
+            raise TypeError('At least one of logger and logger_name is not None')
 
     @staticmethod
     @abstractmethod
-    def define_model(cfg):
+    def define_model(cfg: dict) -> nn.Module:
+        """It must be implement to define the model for training or inference.
+
+        Users can select different models by param in cfg.
+        
+        Args:
+            cfg (dict): config
+
+        Returns:
+            model (nn.Module)
+        """
+
         pass
 
     @staticmethod
     @abstractmethod
-    def build_train_dataset(cfg: dict):
+    def build_train_dataset(cfg: dict) -> Dataset:
+        """It must be implement to build dataset for training.
+
+        Args:
+            cfg (dict): config
+
+        Returns:
+            train dataset (Dataset)
+        """
+
         pass
 
     @staticmethod
     @abstractmethod
     def build_val_dataset(cfg: dict):
+        """It must be implement to build dataset for validation.
+
+        Args:
+            cfg (dict): config
+
+        Returns:
+            val dataset (Dataset)
+        """
+
         pass
 
-    def build_train_data_loader(self, cfg: dict):
+    def build_train_data_loader(self, cfg: dict) -> DataLoader:
+        """Build train dataset and dataloader.
+        Build dataset by calling ```self.build_train_dataset```,
+        build dataloader by calling ```build_data_loader``` or
+        ```build_data_loader_ddp``` when DDP is initialized
+
+        Args:
+            cfg (dict): config
+
+        Returns:
+            train data loader (DataLoader)
+        """
+
         dataset = self.build_train_dataset(cfg)
         if torch.distributed.is_initialized():
             return build_data_loader_ddp(dataset, cfg.TRAIN.DATA)
         else:
             return build_data_loader(dataset, cfg.TRAIN.DATA)
 
-    def build_val_data_loader(self, cfg: dict):
+    def build_val_data_loader(self, cfg: dict) -> DataLoader:
+        """Build val dataset and dataloader.
+        Build dataset by calling ```self.build_train_dataset```,
+        build dataloader by calling ```build_data_loader```.
+
+        Args:
+            cfg (dict): config
+
+        Returns:
+            val data loader (DataLoader)
+        """
+
         dataset = self.build_val_dataset(cfg)
         return build_data_loader(dataset, cfg.VAL.DATA)
 
-    def build_model(self, cfg):
+    def build_model(self, cfg: dict) -> nn.Module:
+        """Build model.
+
+        Initialize model by calling ```self.define_model```,
+        Moves model to the GPU.
+
+        If DDP is initialized, initialize the DDP wrapper.
+
+        Args:
+            cfg (dict): config
+
+        Returns:
+            model (nn.Module)
+        """
+
         model = self.define_model(cfg)
         model = model.cuda()
         if torch.distributed.is_initialized():
             model = DDP(model, device_ids=[get_rank()])
         return model
 
-    def get_ckpt_path(self, epoch: int):
+    def get_ckpt_path(self, epoch: int) -> str:
+        """Get checkpoint path.
+
+        The format is "{ckpt_save_dir}/{model_name}_{epoch}"
+
+        Args:
+            epoch (int): current epoch.
+
+        Returns:
+            checkpoint path (str)
+        """
+
         epoch_str = str(epoch).zfill(len(str(self.num_epochs)))
         ckpt_name = '{}_{}.pt'.format(self.model_name, epoch_str)
         return os.path.join(self.ckpt_save_dir, ckpt_name)
 
-    def save_model(self, epoch):
+    def save_model(self, epoch: int):
+        """Save checkpoint every epoch.
+
+        checkpoint format is {
+            'epoch': current epoch ([1, num_epochs]),
+            'model_state_dict': state_dict of model,
+            'optim_state_dict': state_dict of optimizer
+        }
+
+        Decide whether to delete the last checkpoint by the checkpoint save strategy.
+
+        Args:
+            epoch (int): current epoch.
+        """
+
         ckpt_dict = get_ckpt_dict(self.model, self.optim, epoch)
 
         # backup last epoch
@@ -112,7 +216,19 @@ class Runner(metaclass=ABCMeta):
         if epoch % 10 == 0 or epoch == self.num_epochs:
             clear_ckpt(self.ckpt_save_dir)
 
-    def load_model_resume(self, strict=True):
+    def load_model_resume(self, strict: bool = True):
+        """Load last checkpoint in checkpoint save dir to resume training.
+
+        Load model state dict.
+        Load optimizer state dict.
+        Load start epoch and set it to lr_scheduler.
+
+        Args:
+            strict (bool, optional): whether to strictly enforce that the keys
+                in :attr:`state_dict` match the keys returned by this module's
+                :meth:`~torch.nn.Module.state_dict` function. Default: ``True``
+        """
+
         try:
             checkpoint_dict = load_ckpt(self.ckpt_save_dir, logger=self.logger)
             if isinstance(self.model, DDP):
@@ -127,7 +243,18 @@ class Runner(metaclass=ABCMeta):
         except (IndexError, OSError, KeyError):
             pass
 
-    def load_model(self, ckpt_path=None, strict=True):
+    def load_model(self, ckpt_path: str = None, strict: bool = True):
+        """Load model state dict.
+        if param `ckpt_path` is None, load the last checkpoint in `self.ckpt_save_dir`,
+        else load checkpoint from `ckpt_path`
+
+        Args:
+            ckpt_path (str, optional): checkpoint path, default is None
+            strict (bool, optional): whether to strictly enforce that the keys
+                in :attr:`state_dict` match the keys returned by this module's
+                :meth:`~torch.nn.Module.state_dict` function. Default: ``True``
+        """
+
         try:
             checkpoint_dict = load_ckpt(self.ckpt_save_dir, ckpt_path=ckpt_path, logger=self.logger)
             if isinstance(self.model, DDP):
@@ -137,10 +264,28 @@ class Runner(metaclass=ABCMeta):
         except (IndexError, OSError):
             raise OSError('Ckpt file does not exist')
 
-    def train(self, cfg):
+    def train(self, cfg: dict):
+        """Train model.
+
+        Train process:
+        [init_training]
+        for in train_epoch
+            [on_epoch_start]
+            for in train iters
+                [train_iters]
+            [on_epoch_end] ------> Epoch Val: val every n epoch
+                                    for in val iters
+                                        val iter
+                                    [on_validation_end]
+        [on_training_end]
+
+        Args:
+            cfg (dict): config
+        """
+
         self.init_training(cfg)
 
-        # train loop
+        # training loop
         for epoch_index in range(self.start_epoch, self.num_epochs):
             epoch = epoch_index + 1
             self.on_epoch_start(epoch)
@@ -158,41 +303,6 @@ class Runner(metaclass=ABCMeta):
             epoch_end_time = time.time()
             self.on_epoch_end(epoch, epoch_end_time - epoch_start_time)
         self.on_training_end()
-
-    @abstractmethod
-    def train_iters(self, epoch, iter_index, data):
-        pass
-
-    def backward(self, loss):
-        self.optim.zero_grad()
-        loss.backward()
-        self.optim.step()
-
-    @torch.no_grad()
-    @master_only
-    def validate(self, cfg: dict = None, train_epoch: int = None):
-        if train_epoch is None:
-            self.init_validation(cfg)
-
-        self.model.eval()
-        val_start_time = time.time()
-
-        # val loop
-        for iter_index, data in enumerate(self.val_data_loader):
-            self.val_iters(iter_index, data)
-
-        val_end_time = time.time()
-        self.update_epoch_meter('val_time', val_end_time - val_start_time)
-        # print val meters
-        self.print_epoch_meters('val')
-        if train_epoch is not None:
-            val_interval = self.val_interval if self.val_interval is not None else 1
-            # tensorboard plt meters
-            self.plt_epoch_meters('val', train_epoch // val_interval)
-
-    @abstractmethod
-    def val_iters(self, iter_index, data):
-        pass
 
     def init_training(self, cfg):
         # init training param
@@ -243,13 +353,6 @@ class Runner(metaclass=ABCMeta):
             self.init_validation(cfg)
 
     @master_only
-    def init_validation(self, cfg):
-        if hasattr(cfg.VAL, 'INTERVAL'):
-            self.val_interval = cfg.VAL.INTERVAL
-        self.val_data_loader = self.build_val_data_loader(cfg)
-        self.register_epoch_meter('val_time', 'val', '{:.2f} (s)', plt=False)
-
-    @master_only
     def on_epoch_start(self, epoch):
         # print epoch num
         self.logger.info('epoch {:d} / {:d}'.format(epoch, self.num_epochs))
@@ -277,6 +380,48 @@ class Runner(metaclass=ABCMeta):
         if is_master():
             # close tensorboard writer
             self.tensorboard_writer.close()
+
+    @abstractmethod
+    def train_iters(self, epoch, iter_index, data):
+        pass
+
+    def backward(self, loss):
+        self.optim.zero_grad()
+        loss.backward()
+        self.optim.step()
+
+    @torch.no_grad()
+    @master_only
+    def validate(self, cfg: dict = None, train_epoch: int = None):
+        if train_epoch is None:
+            self.init_validation(cfg)
+
+        self.model.eval()
+        val_start_time = time.time()
+
+        # val loop
+        for iter_index, data in enumerate(self.val_data_loader):
+            self.val_iters(iter_index, data)
+
+        val_end_time = time.time()
+        self.update_epoch_meter('val_time', val_end_time - val_start_time)
+        # print val meters
+        self.print_epoch_meters('val')
+        if train_epoch is not None:
+            val_interval = self.val_interval if self.val_interval is not None else 1
+            # tensorboard plt meters
+            self.plt_epoch_meters('val', train_epoch // val_interval)
+
+    @master_only
+    def init_validation(self, cfg):
+        if hasattr(cfg.VAL, 'INTERVAL'):
+            self.val_interval = cfg.VAL.INTERVAL
+        self.val_data_loader = self.build_val_data_loader(cfg)
+        self.register_epoch_meter('val_time', 'val', '{:.2f} (s)', plt=False)
+
+    @abstractmethod
+    def val_iters(self, iter_index, data):
+        pass
 
     @master_only
     def register_epoch_meter(self, name, meter_type, fmt='{:f}', plt=True):
