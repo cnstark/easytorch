f07b77a5377a3357d534790f899bb5690482affd
Yuhao Wang <yuhaow97@gmail.com>
2021-07-22 16:02:18 +0800

    feature: mnist example

---
+++ examples/mnist/mnist_runner.py
@@ -0,0 +1,68 @@
+from torch import nn
+import torchvision
+
+from easytorch import Runner
+
+from conv_net import ConvNet
+
+
+class MNISTRunner(Runner):
+    def __init__(self, cfg: dict, use_gpu: bool):
+        super().__init__(cfg, use_gpu)
+        self.loss = None
+
+    def init_training(self, cfg):
+        super().init_training(cfg)
+
+        self.loss = nn.NLLLoss()
+        self.loss = self.to_running_device(self.loss)
+
+        self.register_epoch_meter('train_loss', 'train', '{:.2f}')
+        self.register_epoch_meter('val_loss', 'val', '{:.2f}')
+
+    @staticmethod
+    def define_model(cfg: dict) -> nn.Module:
+        return {
+            'conv_net': ConvNet
+        }[cfg['MODEL']['NAME']](**cfg['MODEL'].get('PARAM', {}))
+
+    @staticmethod
+    def build_train_dataset(cfg: dict):
+        return torchvision.datasets.MNIST(
+            cfg['TRAIN']['DATA']['DIR'], train=True, download=True,
+            transform=torchvision.transforms.Compose([
+                torchvision.transforms.ToTensor(),
+                torchvision.transforms.Normalize(
+                    (0.1307,), (0.3081,))
+            ])
+        )
+
+    @staticmethod
+    def build_val_dataset(cfg: dict):
+        return torchvision.datasets.MNIST(
+            cfg['VAL']['DATA']['DIR'], train=False, download=True,
+            transform=torchvision.transforms.Compose([
+                torchvision.transforms.ToTensor(),
+                torchvision.transforms.Normalize(
+                    (0.1307,), (0.3081,))
+            ])
+        )
+
+    def train_iters(self, epoch, iter_index, data):
+        _input, _target = data
+        _input = _input.cuda()
+        _target = _target.cuda()
+
+        output = self.model(_input)
+        loss = self.loss(output, _target)
+        self.update_epoch_meter('train_loss', loss.item())
+        return loss
+
+    def val_iters(self, iter_index, data):
+        _input, _target = data
+        _input = _input.cuda()
+        _target = _target.cuda()
+
+        output = self.model(_input)
+        loss = self.loss(output, _target)
+        self.update_epoch_meter('val_loss', loss.item())
---
+++ examples/mnist/train.py
@@ -0,0 +1,17 @@
+from argparse import ArgumentParser
+
+from easytorch import launch_training
+
+
+def parse_args():
+    parser = ArgumentParser(description='Welcome to EasyTorch!')
+    parser.add_argument('-c', '--cfg', help='training config', required=True)
+    parser.add_argument('--gpus', help='visible gpus', type=str)
+    parser.add_argument('--tf32', help='enable tf32 on Ampere device', action='store_true')
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    launch_training(args.cfg, args.gpus, args.tf32)
