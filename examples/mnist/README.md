6edd8338721e8e081ea9d00373bcd28a6cabadb2
Yuhao Wang <yuhaow97@gmail.com>
2021-07-24 01:10:01 +0800

    feature: MNIST doc & readme

---
+++ examples/mnist/README.md
@@ -0,0 +1,15 @@
+# EasyTorch Example - MNIST Classification
+
+## Train
+
+* CPU
+
+```shell
+python train.py -c config\mnist_cpu_cfg.py
+```
+
+* GPU (1x)
+
+```shell
+python train.py -c config\mnist_1x_cfg.py
+```
--- examples/mnist/mnist_runner.py
+++ examples/mnist/mnist_runner.py
@@ -7,11 +7,15 @@ from conv_net import ConvNet
 
 
 class MNISTRunner(Runner):
-    def __init__(self, cfg: dict, use_gpu: bool):
-        super().__init__(cfg, use_gpu)
-        self.loss = None
-
     def init_training(self, cfg):
+        """Initialize training.
+
+        Including loss, meters, etc.
+
+        Args:
+            cfg (dict): config
+        """
+
         super().init_training(cfg)
 
         self.loss = nn.NLLLoss()
@@ -23,12 +27,33 @@ class MNISTRunner(Runner):
 
     @staticmethod
     def define_model(cfg: dict) -> nn.Module:
+        """Define model.
+
+        If you have multiple models, insert the name and class into the dict below,
+        and select it through ```config```.
+
+        Args:
+            cfg (dict): config
+
+        Returns:
+            model (nn.Module)
+        """
+
         return {
             'conv_net': ConvNet
         }[cfg['MODEL']['NAME']](**cfg['MODEL'].get('PARAM', {}))
 
     @staticmethod
     def build_train_dataset(cfg: dict):
+        """Build MNIST train dataset
+
+        Args:
+            cfg (dict): config
+
+        Returns:
+            train dataset (Dataset)
+        """
+
         return torchvision.datasets.MNIST(
             cfg['TRAIN']['DATA']['DIR'], train=True, download=True,
             transform=torchvision.transforms.Compose([
@@ -40,6 +65,15 @@ class MNISTRunner(Runner):
 
     @staticmethod
     def build_val_dataset(cfg: dict):
+        """Build MNIST val dataset
+
+        Args:
+            cfg (dict): config
+
+        Returns:
+            train dataset (Dataset)
+        """
+
         return torchvision.datasets.MNIST(
             cfg['VAL']['DATA']['DIR'], train=False, download=True,
             transform=torchvision.transforms.Compose([
@@ -50,6 +84,17 @@ class MNISTRunner(Runner):
         )
 
     def train_iters(self, epoch, iter_index, data):
+        """Training details.
+
+        Args:
+            epoch (int): current epoch.
+            iter_index (int): current iter.
+            data (torch.Tensor or tuple): Data provided by DataLoader
+
+        Returns:
+            loss (torch.Tensor)
+        """
+
         _input, _target = data
         _input = self.to_running_device(_input)
         _target = self.to_running_device(_target)
@@ -60,6 +105,13 @@ class MNISTRunner(Runner):
         return loss
 
     def val_iters(self, iter_index, data):
+        """Validation details.
+
+        Args:
+            iter_index (int): current iter.
+            data (torch.Tensor or tuple): Data provided by DataLoader
+        """
+
         _input, _target = data
         _input = self.to_running_device(_input)
         _target = self.to_running_device(_target)
