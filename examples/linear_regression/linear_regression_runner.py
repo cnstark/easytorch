3edff26fe11f47950ed5e2ab0efcbaea95d5f93f
Yuhao Wang <yuhaow97@gmail.com>
2021-07-24 18:22:47 +0800

    feature: remove val

--- examples/linear_regression/linear_regression_runner.py
+++ examples/linear_regression/linear_regression_runner.py
@@ -17,7 +17,6 @@ class LinearRegressionRunner(Runner):
         self.loss = self.to_running_device(self.loss)
 
         self.register_epoch_meter('train_loss', 'train', '{:.2f}')
-        self.register_epoch_meter('val_loss', 'val', '{:.2f}')
 
     @staticmethod
     def define_model(cfg: dict) -> nn.Module:
@@ -31,14 +30,6 @@ class LinearRegressionRunner(Runner):
             cfg['TRAIN']['DATA']['NUM'],
         )
 
-    @staticmethod
-    def build_val_dataset(cfg: dict):
-        return LinearDataset(
-            cfg['VAL']['DATA']['K'],
-            cfg['VAL']['DATA']['B'],
-            cfg['VAL']['DATA']['NUM'],
-        )
-
     def train_iters(self, epoch, iter_index, data):
         x, y = data
         x = self.to_running_device(x)
@@ -48,15 +39,6 @@ class LinearRegressionRunner(Runner):
         loss = self.loss(output, y)
         self.update_epoch_meter('train_loss', loss.item())
         return loss
-
-    def val_iters(self, iter_index, data):
-        x, y = data
-        x = self.to_running_device(x)
-        y = self.to_running_device(y)
-
-        output = self.model(x)
-        loss = self.loss(output, y)
-        self.update_epoch_meter('val_loss', loss.item())
 
     def on_training_end(self):
         super().on_training_end()
