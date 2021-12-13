fdf5660d004fc47552f478bfca0c4e03c2a18ec9
Yuhao Wang <yuhaow97@gmail.com>
2021-12-13 17:30:37 +0800

    fix: fix seed on DDP

--- easytorch/utils/env.py
+++ easytorch/utils/env.py
@@ -53,7 +53,16 @@ def setup_random_seed(seed: int):
     """
     random.seed(seed)
     np.random.seed(seed)
+
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    os.environ['CUBLAS_WORKSPACE_CONFIG'] = ':4096:8'
+
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
+
+    torch.use_deterministic_algorithms(True)
     torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.enabled = False
+    torch.backends.cudnn.benchmark = False
+
