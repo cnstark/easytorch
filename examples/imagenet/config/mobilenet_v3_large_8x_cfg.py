5f22b82a0a670e67ae51a569ea060c6c5d1f7469
Yuhao Wang <yuhaow97@gmail.com>
2022-01-14 00:45:01 +0800

    feature: mobilenet_v3_large

---
+++ examples/imagenet/config/mobilenet_v3_large_8x_cfg.py
@@ -0,0 +1,53 @@
+import os
+from easydict import EasyDict
+
+from imagenet_runner import ImagenetRunner
+
+CFG = EasyDict()
+
+CFG.DESC = 'imagenet mobilenet_v3_large'
+CFG.RUNNER = ImagenetRunner
+CFG.GPU_NUM = 8
+
+CFG.MODEL = EasyDict()
+CFG.MODEL.NAME = 'mobilenet_v3_large'
+
+CFG.TRAIN = EasyDict()
+
+CFG.TRAIN.NUM_EPOCHS = 90
+CFG.TRAIN.CKPT_SAVE_DIR = os.path.join(
+    'checkpoints',
+    '_'.join([CFG.MODEL.NAME, str(CFG.TRAIN.NUM_EPOCHS)])
+)
+CFG.TRAIN.CKPT_SAVE_STRATEGY = None
+
+CFG.TRAIN.OPTIM = EasyDict()
+CFG.TRAIN.OPTIM.TYPE = 'SGD'
+CFG.TRAIN.OPTIM.PARAM = {
+    'lr': 0.01,
+    'momentum': 0.9,
+    'weight_decay': 1e-4
+}
+
+CFG.TRAIN.LR_SCHEDULER = EasyDict()
+CFG.TRAIN.LR_SCHEDULER.TYPE = 'StepLR'
+CFG.TRAIN.LR_SCHEDULER.PARAM = {
+    'step_size': 30,
+    'gamma': 0.1
+}
+
+IMAGENET_PATH = '/path/to/imagenet/jpegs'
+
+CFG.TRAIN.DATA = EasyDict()
+CFG.TRAIN.DATA.BATCH_SIZE = 4
+CFG.TRAIN.DATA.NUM_WORKERS = 4
+CFG.TRAIN.DATA.SHUFFLE = True
+
+CFG.TRAIN.DATA.DIR = os.path.join(IMAGENET_PATH, 'train')
+
+CFG.VAL = EasyDict()
+
+CFG.VAL.INTERVAL = 1
+
+CFG.VAL.DATA = EasyDict()
+CFG.VAL.DATA.DIR = os.path.join(IMAGENET_PATH, 'val')
