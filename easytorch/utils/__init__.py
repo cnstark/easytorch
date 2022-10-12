3f650662eedcc06bbd70b13697fa63ac2b24ebd9
Yuhao Wang <yuhaow97@gmail.com>
2022-10-12 21:19:28 +0800

    fix

--- easytorch/utils/__init__.py
+++ easytorch/utils/__init__.py
@@ -1,5 +1,5 @@
 from .env import set_visible_devices, set_tf32_mode, setup_determinacy, set_env
-from .device import get_device_type, set_device_type, get_device_count, set_device, to_device, set_device_manual_seed
+from .device import get_device_type, set_device_type, get_device_count, set_device, to_device
 from .dist import get_rank, get_local_rank, get_world_size, is_rank, is_master, master_only
 from .logging import get_logger
 from .named_hook import NamedForwardHook, NamedBackwardHook
@@ -7,7 +7,6 @@ from .timer import Timer, TimePredictor
 
 __all__ = [
     'set_visible_devices', 'set_tf32_mode', 'setup_determinacy', 'set_env', 'get_device_type', 'set_device_type',
-    'get_device_count', 'set_device', 'to_device', 'set_device_manual_seed', 'get_rank', 'get_local_rank',
-    'get_world_size', 'is_rank', 'is_master', 'master_only', 'get_logger', 'NamedForwardHook', 'NamedBackwardHook',
-    'Timer', 'TimePredictor'
+    'get_device_count', 'set_device', 'to_device', 'get_rank', 'get_local_rank', 'get_world_size', 'is_rank',
+    'is_master', 'master_only', 'get_logger', 'NamedForwardHook', 'NamedBackwardHook', 'Timer', 'TimePredictor'
 ]
